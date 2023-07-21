7bfed1cc2cbd5e5a7d312dee5de579d460b05e98
BISWAJIT BOSE <97077410+01edie@users.noreply.github.com>
2023-07-21 13:53:17 +0530

    prelim

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,158 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Grid,
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  IconButton,
+  CardHeader,
+} from "@mui/material";
+import CallMissedOutgoingIcon from "@mui/icons-material/CallMissedOutgoing";
+import InfoIcon from "@mui/icons-material/Info";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Banner from "./components/banner";
+// import dhanush from './assets/dhanush.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Box sx={{ height: 70, bgcolor: "#3D7BF8" }} />
+      {/* 59F83D */}
+      <Grid container xs={12} p={3} spacing={2}>
+        <Grid item xs={12}>
+          <Banner />
+        </Grid>
+        <Grid item xs={12} container spacing={2} mt={2}>
+          <Grid item xs={4}>
+            <Card sx={{ boxShadow: 5 }}>
+              <CardHeader
+                avatar={
+                  <img
+                    src="/dhanush.png"
+                    style={{ objectFit: "fill", width: "70px", height: "auto" }}
+                  />
+                }
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                sx={{ mt: "10px" }}
+                title="Title 1"
+                subheader="July 15, 2023"
+              />
+              <CardContent>
+                <Typography>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Perspiciatis labore asperiores facilis perferendis distinctio
+                  cumque sit numquam, in voluptates hic cum iure nisi nemo ex
+                  incidunt molestias natus iste id ipsam quo corporis.
+                  Necessitatibus mollitia repellat eos, nulla laudantium
+                  consequuntur, libero ratione error sint est ex quia possimus
+                  quis nisi?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                {/* <Button size="small" variant="outlined">Learn More</Button> */}
+                <IconButton>
+                  <InfoIcon />
+                </IconButton>
+                <IconButton>
+                  <CallMissedOutgoingIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card sx={{ boxShadow: 5 }}>
+              <CardHeader
+                avatar={
+                  <img
+                    src="/B2P.png"
+                    style={{ objectFit: "fill", width: "auto", height: "40px" }}
+                  />
+                }
+                sx={{
+                  mt: "10px",
+                  mb:'20px'
+                }}
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                title="Title 2"
+                subheader="June 23, 2023"
+              />
+              <CardContent>
+                <Typography>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Perspiciatis labore asperiores facilis perferendis distinctio
+                  cumque sit numquam, in voluptates hic cum iure nisi nemo ex
+                  incidunt molestias natus iste id ipsam quo corporis.
+                  Necessitatibus mollitia repellat eos, nulla laudantium
+                  consequuntur, libero ratione error sint est ex quia possimus
+                  quis nisi?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                {/* <Button size="small" variant="outlined">Learn More</Button> */}
+                <IconButton>
+                  <InfoIcon />
+                </IconButton>
+                <IconButton>
+                  <CallMissedOutgoingIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card sx={{ boxShadow: 5 }}>
+              <CardHeader
+                avatar={
+                  <img
+                    src="/dhanush.png"
+                    style={{ objectFit: "fill", width: "70px", height: "auto" }}
+                  />
+                }
+                sx={{ mt: "10px" }}
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                title="Title 3"
+                subheader="July 21, 2016"
+              />
+              <CardContent>
+                <Typography>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Perspiciatis labore asperiores facilis perferendis distinctio
+                  cumque sit numquam, in voluptates hic cum iure nisi nemo ex
+                  incidunt molestias natus iste id ipsam quo corporis.
+                  Necessitatibus mollitia repellat eos, nulla laudantium
+                  consequuntur, libero ratione error sint est ex quia possimus
+                  quis nisi?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                {/* <Button size="small" variant="outlined">Learn More</Button> */}
+                <IconButton>
+                  <InfoIcon />
+                </IconButton>
+                <IconButton>
+                  <CallMissedOutgoingIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
---
+++ src/components/banner/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { Box, Typography } from "@mui/material";
+/* eslint-enable */
+
+import { FreeMode, Pagination, Autoplay } from "swiper";
+
+
+
+type Props = {};
+
+export default function Banner({}: Props) {
+  return (
+    <Box  mt={1} >
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        // freeMode
+        pagination
+        grabCursor
+        autoplay={{
+          delay: 2000,
+          pauseOnMouseEnter: true,
+        }}
+        loop
+        modules={[FreeMode, Pagination, Autoplay]}
+        className="banner"
+      >
+        {/* slidesPerView attribute requiring  <= SwiperSlides/2 to be in loop */}
+        <SwiperSlide>
+            <img src='/image1.jpg' style={{ objectFit: 'fill', width:'100%', height:'400px', borderRadius:'10px'}}  alt="" />
+            <Typography variant="h6">Caption 1</Typography>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src='/image2.jpg' style={{ objectFit: 'fill', width:'100%', height:'400px', borderRadius:'10px'}} alt="" />
+            <Typography variant="h6">Caption 2</Typography>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src='/image3.jpg' style={{ objectFit: 'fill', width:'100%', height:'400px', borderRadius:'10px'}} alt="" />
+            <Typography variant="h6">Caption 3</Typography>
+        </SwiperSlide>
+      </Swiper>
+    </Box>
+  );
+}
